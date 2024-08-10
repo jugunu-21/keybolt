b1b74cd17e57746e1b28afe2f011f55c350db0a4
jugunu-21 <aaruhijugunu@gmail.com>
2024-08-11 02:28:30 +0530

    changes the newsletter coponent and added hero section

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,19 +1,20 @@
 
 import {links} from "@/public/navdata"
 import {RoundedDrawerNav} from "../navbar/roundedDrawerNav/roundedDrawerNav"
+import { Hero } from "../hero/hero";
 export const RoundedDrawerNavExample = () => {
 
     return (
-        <div className="bg-neutral-950">
+        <div className="bg-neutral-950 ">
             <RoundedDrawerNav
                 links={links}
                 navBackground="bg-neutral-950"
                 bodyBackground="bg-white"
             >
-                <div className="flex flex-col items-center justify-center px-12 py-32">
-                    <p className="text-center">
-                        Your hero section content goes here {":)"}
-                    </p>
+                <div className="flex flex-col items-center justify-center">
+                    <div> <Hero/></div>
+                   
+                   
                 </div>
             </RoundedDrawerNav>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,9 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 } satisfies Config
 
 export default config
