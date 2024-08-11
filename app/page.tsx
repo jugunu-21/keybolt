fd7e157a1664e72747dcb13a32e6dfe0b99ee617
jugunu-21 <aaruhijugunu@gmail.com>
2024-08-11 15:16:24 +0530

    services and about component

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,22 @@ import { HeroParallaxDemo } from "../components/heroParallaxDemo"
 import { ImagePartion } from "../components/imagepartition"
 import {BlogPostCarousel} from "../components/blogPostCarousel/blogPostCarousel"
 import {SparklesPreview} from "../components/sparkelPreview/sparkelPreview"
+import {BasicFAQ} from "../components/faq/faq"
+import {ShiftingContactForm} from "../components/contact/contact"
+import {RevealBento} from "../components/servicesAndAbout/servicesAndAbout"
+import { Feature } from "@/components/feauture/feature";
 export default function Home() {
   return (
     <main>
       <RoundedDrawerNavExample />
       <ImagePartion />
+      <Feature/>
       <SparklesPreview/>
       <BlogPostCarousel/>
       <HeroParallaxDemo />
+      <RevealBento/>
+      <ShiftingContactForm/>
+      <BasicFAQ/>
     </main>
   );
 }
