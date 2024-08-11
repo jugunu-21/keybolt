f597d20125c3ba2814a005133ccfeb4840a96750
jugunu-21 <aaruhijugunu@gmail.com>
2024-08-11 19:43:41 +0530

    images in contact us

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -171,8 +171,7 @@ const Imagess = ({ selected }: { selected: "company" | "individual" }) => {
         transition={BASE_TRANSITION}
         className="absolute inset-0 bg-slate-200"
         style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80)",
+          backgroundImage: `url(/images/joinus.jpg)`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
@@ -185,8 +184,7 @@ const Imagess = ({ selected }: { selected: "company" | "individual" }) => {
         transition={BASE_TRANSITION}
         className="absolute inset-0 bg-slate-200"
         style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80)",
+          backgroundImage: `url(/images/joinus.jpg)`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
--- public/images/images.ts
+++ public/images/images.ts
@@ -1,12 +1,10 @@
-import image1 from "@/public/images/pexels-anna-nekrashevich-6802045.jpg"
-import image2 from "@/public/images/pexels-anna-nekrashevich-6802048.jpg"
-import image3 from "@/public/images/pexels-eugeniofr-17070805.jpg"
-import image4 from "@/public/images/pexels-goumbik-590016.jpg"
+import image1 from "@/public/images/featureImage1.jpg"
+import image2 from "@/public/images/featureImage2.jpg"
+import image3 from "@/public/images/featureImage3.jpg"
+import image4 from "@/public/images/featureImage4.jpg"
 export const imagesFeature = {
     image1:image1,
     image2:image2,
     image3:image3,
     image4:image4,
-   
-
 }
