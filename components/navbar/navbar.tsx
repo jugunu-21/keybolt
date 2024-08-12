941ed156c29b0101dca2ab96f6cfaf60a2b30255
jugunu-21 <aaruhijugunu@gmail.com>
2024-08-12 18:54:36 +0530

    chnages in navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -41,7 +41,7 @@ export const NavExample = () => {
                         <div className=" flex items-center sm:hidden">
                             {/* <!-- Mobile menu button -->*/}
 
-                            <button onClick={() => setIsMenuOpen(!isMenuOpen)} type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none " aria-controls="mobile-menu" aria-expanded="false">
+                            <button  type="button" className=" hideenrelative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none " aria-controls="mobile-menu" aria-expanded="false">
 
                                 <div className="">
 
@@ -52,20 +52,15 @@ export const NavExample = () => {
                             </button>
                         </div>
 
-                        <div onMouseEnter={() => setIsMenuOpen(true)}
-                            onMouseLeave={() => setIsMenuOpen(false)} className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-
+                        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                             <div className="hidden sm:ml-6 sm:block">
                                 <div className="flex space-x-4">
-                                    {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"  -->*/}
-                                    <a onClick={() => setIsMenuOpen(false)} href="/" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Dashboard</a>
+                                    <a  href="/" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Dashboard</a>
                                     {blank&&<>
-                                        <a onClick={() => setIsMenuOpen(false)} href="#testimonial" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Testimonial</a>
-                                    <a onClick={() => setIsMenuOpen(false)} href="#feature" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Feature</a>
+                                        <a  href="#testimonial" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Testimonial</a>
+                                    <a href="#feature" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Feature</a>
                                     </> }
-                         
-                                    
-                                    <a onClick={() => setIsMenuOpen(false)} href="/contact" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact Us</a>
+                                    <a  href="/contact" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact Us</a>
                                 </div>
                             </div>
                         </div>
@@ -107,8 +102,9 @@ export const NavExample = () => {
                                 {blank&&<> <a href="#testimonial" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Testimonials</a>
                                 <a  href="#feature" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Features</a>
                                 
-                                <a href="/contact" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact Us</a>
+                              
                            </>}
+                           <a href="/contact" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact Us</a>
                             </>)}
 
                     </div>
