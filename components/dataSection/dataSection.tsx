f7e075dd1c2a37948e5217429f83ec5fabb30a28
jugunu-21 <aaruhijugunu@gmail.com>
2024-08-12 14:05:44 +0530

    changes in data content

--- components/dataSection/dataSection.tsx
+++ components/dataSection/dataSection.tsx
@@ -11,53 +11,46 @@ export default function Section() {
         <>
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
-                    <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+                    <div className="flex items-center lg:w-4/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
                         <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                            <Image className="w-10" src="/svg/undraw_visual_data_re_mxxo.svg" alt="Description" width={100} height={100}></Image>
+                            <Image className="w-40" src="/svg/undraw_visual_data_re_mxxo.svg" alt="Description" width={100} height={100}></Image>
                         </div>
                         <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">Shooting Stars</h2>
-                            <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                            <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">Transform Your Food Business with AI</h2>
+                            <p className="leading-relaxed text-base">Keybolt is revolutionizing the food industry by making advanced AI tools accessible to all. Our platform helps businesses streamline operations, optimize inventory, and make data-driven decisions that boost efficiency and profitability. With Keybolt, even the smallest local shops can leverage cutting-edge technology to thrive in a competitive market..</p>
+                            {/* <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
-                            </a>
+                            </a> */}
                         </div>
                     </div>
                     <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
                         <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">The Catalyzer</h2>
-                            <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                            <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">Smarter Operations, Stronger Growth</h2>
+                            <p className="leading-relaxed text-base">At Keybolt, we understand that running a food business is demanding. That’s why we’ve created a digital assistant that sits in your pocket, helping you manage your most critical tasks effortlessly. From automated ordering to real-time sales monitoring, Keybolt ensures your operations run smoothly, giving you more time to grow your business..</p>
+                            {/* <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
-                            </a>
+                            </a> */}
                         </div>
                         <div className="sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
-                                <circle cx="6" cy="6" r="3"></circle>
-                                <circle cx="6" cy="18" r="3"></circle>
-                                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                            </svg>
+                        <Image className="w-40" src="/svg/undraw_start_building_re_xani (1).svg" alt="Description" width={100} height={100}></Image>
                         </div>
                     </div>
                     <div className="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col">
                         <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
-                                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                                <circle cx="12" cy="7" r="4"></circle>
-                            </svg>
+                        <Image className="w-40" src="/svg/undraw_cooking_p7m1 (1).svg" alt="Description" width={100} height={100}></Image>
                         </div>
                         <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">The 400 Blows</h2>
-                            <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                            <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2"> Empowering Local Entrepreneurs</h2>
+                            <p className="leading-relaxed text-base">We believe in the power of local businesses to create vibrant communities. Keybolt is designed to empower mom-and-pop shops, local chains, and independent eateries with the tools they need to succeed. Our easy-to-use platform simplifies complex tasks, so you can focus on what you do best—serving your customers..</p>
+                            {/* <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
-                            </a>
+                            </a> */}
                         </div>
                     </div>
                     <div className="flex justify-center items-center py-6 my-8">
