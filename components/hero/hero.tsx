445fe000b02b8419e44d899f03950b2cb9064f89
jugunu-21 <aaruhijugunu@gmail.com>
2024-08-13 05:37:45 +0530

    small bug

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -123,9 +123,9 @@ const Copy = () => {
     <>
       <div className="mb-1.5 rounded-full bg-zinc-600">
         <a
-          href="#"
-          target="_blank"
-          rel="nofollow"
+          href="/"
+          // target="_blank"
+          // rel="nofollow"
           className="flex origin-top-left items-center rounded-full border border-zinc-900 bg-white p-0.5 text-sm transition-transform hover:-rotate-2"
         >
           <span className="rounded-full bg-[#d44a3e] px-2 py-0.5 font-medium text-white">
