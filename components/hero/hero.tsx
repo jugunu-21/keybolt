87d55cf9f990cb3a9ac0866cb0eff9dd829b8092
jugunu-21 <aaruhijugunu@gmail.com>
2024-10-22 22:29:57 +0530

    change the size of product hunt badge

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -53,21 +53,19 @@ const Copy = () => {
           href="/"
           // target="_blank"
           // rel="nofollow"
-          className="flex origin-top-left items-center rounded-full border border-zinc-900 bg-white p-0.5 text-sm transition-transform hover:-rotate-2"
+          className="flex origin-top-left items-center rounded-xl border border-zinc-900 bg-white p-0.5 text-sm transition-transform hover:-rotate-2"
         >
-          <span className="rounded-full bg-[#d44a3e] px-2 py-0.5 font-medium text-white">
-            HEY!
-          </span>
-          <span className=" pr-1 inline-block"> 
+
+          <span className="p-1 inline-flex w-full">
             <Image
               src="/producthunt.png"
               alt="Your Company Name"
               width={100}
-              height={100 }
-              className=" ml-1 h-fit"
+              height={100}
+              className=" h-8 md:h-12 w-full"
             />
           </span>
-          <FiArrowUpRight className="mr-2 inline-block" />
+
         </a>
       </div>
       <h1 className="max-w-4xl text-center text-3xl font-black leading-[1.15] md:text-5xl md:leading-[1.15]">
